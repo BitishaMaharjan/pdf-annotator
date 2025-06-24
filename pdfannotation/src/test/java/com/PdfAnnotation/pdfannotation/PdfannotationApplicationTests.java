74dabd908a1660750e3d43a6d54f3a84f734c4c9
Bitisha Maharjan <bitisha.maharjan@bajratechnologies.com>
2025-06-24 22:51:32 +0545

    feat: initialize the project

---
+++ pdfannotation/src/test/java/com/PdfAnnotation/pdfannotation/PdfannotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PdfAnnotation.pdfannotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfannotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
